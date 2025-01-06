107c65bd2e4613413fd5ca399886e3c1ab8fd6ed
Georgia Mason <140232973+georgiapoppy@users.noreply.github.com>
2025-01-06 10:51:43 +1030

    Update Human.h

--- Human.h
+++ Human.h
@@ -10,11 +10,15 @@ using namespace std;
 class Human : public Player {
        private:
           string name;
+          vector<string> users;
+          vector<int> highscores;
        public:
           Human();
           Human(string name1);
           string makeMove() override;
           string getName() override;
+          void addUser();
+          void addHighscore;
 };
 
 #endif
